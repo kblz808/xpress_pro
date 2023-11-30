f982c0c64c5049579917621b3f1c5e68de90b230
yeabsera_seyoum <yeabsera.seyoum@bitscollege.edu.et>
2023-11-30 14:00:30 +0300

    Navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="main">
+        <img src="../assets/logo.png" alt="">        
+            <nav>
+                <ul href="#">Home</ul>
+                <ul href="#">Cars</ul>
+                <ul href="#">Booking</ul>
+                <ul href="#">My Account</ul>
+                <ul href="#">About Us <i class='bx bx-chevron-down'></i></ul>               
+                <ul href="#">Blog</ul>               
+            </nav>       
+        <vs-button color="#1ECB15">Sign in</vs-button>       
+    </div>
+
+</template>
+
+<style scoped>
+    .main {
+        background-color: black;
+        display: flex;
+        justify-content: space-between;
+        padding: 15px 120px;
+        align-items: center;
+
+    }
+    nav{
+        display: flex;
+        gap: 30px; 
+    }
+     nav ul {
+        color: white;
+        cursor: pointer;
+    }
+</style>
