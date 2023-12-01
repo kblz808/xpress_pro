48b323a23ee2973331920656d15042e2393c2b99
yeabsera_seyoum <yeabsera.seyoum@bitscollege.edu.et>
2023-12-01 14:33:00 +0300

    Features section

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,18 +2,21 @@
     <Banner />
     <Hero />
     <Fleet />
+    <Feature />
 </template>
 
 <script>
   import Banner from '../components/Banner.vue'
   import Fleet from '../components/Fleet.vue'
   import Hero from '../components/Hero.vue'
+  import Feature from '../components/Features.vue'
 
   export default {
     components: {
       Banner,
       Fleet,
       Hero,
+      Feature,
     }
   }
 </script>
