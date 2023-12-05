c8b4b6791ff2b22c9cabbc75351bb9550fd6d6aa
kaleab <808kaleabkzin@gmail.com>
2023-12-05 16:26:49 +0300

    added cars filter page with state management

--- src/pages/CarsFilter.vue
+++ src/pages/CarsFilter.vue
@@ -13,7 +13,6 @@
     
       <div class="filter car_type">
         <h3>Vehicle Type</h3>
-
         <vs-checkbox-group v-model="vehicle_type">
           <vs-checkbox value="car"> Car </vs-checkbox>
           <vs-checkbox value="van"> Van </vs-checkbox>
@@ -22,11 +21,40 @@
         </vs-checkbox-group>
       </div>
 
+      <div class="filter body_type">
+        <h3>Car Body Type</h3>
+        <vs-checkbox-group v-model="body_type">
+          <vs-checkbox value="convertible"> Convertible </vs-checkbox>
+          <vs-checkbox value="coupe"> Coupe </vs-checkbox>
+          <vs-checkbox value="exotic_cars"> Exotic Cars </vs-checkbox>
+          <vs-checkbox value="hatchback"> Hatch Back </vs-checkbox>
+          <vs-checkbox value="minivan"> Mini Van </vs-checkbox>
+          <vs-checkbox value="truck"> Truck </vs-checkbox>
+          <vs-checkbox value="sedan"> Sedan </vs-checkbox>
+          <vs-checkbox value="sports_car"> Sports Car </vs-checkbox>
+          <vs-checkbox value="station_wagon"> Station Wagon </vs-checkbox>
+          <vs-checkbox value="wagon"> Wagon </vs-checkbox>
+        </vs-checkbox-group>
+      </div>
+
+      <div class="filter car_seats">
+        <h3>Car Seats</h3>
+        <vs-checkbox-group v-model="car_seats">
+          <vs-checkbox value="2"> 2 seats </vs-checkbox>
+          <vs-checkbox value="4"> 4 seats </vs-checkbox>
+          <vs-checkbox value="6"> 6 seats </vs-checkbox>
+        </vs-checkbox-group>
+      </div>
+      
+      <div class="filter price">
+        <h3> Minimum Price </h3>
+
+      </div>
     </div>
 
     <div class="cars">
-      <template v-for="car in vehicles">
-        <ApiCarCards :car=car />
+      <template v-for="car in cars">
+        <FilterCarCard :car=car />
       </template>
     </div>
   </div>
@@ -37,7 +65,7 @@
 <script>
 import Banner from '../components/Banner.vue'
 import NavBar from '../components/NavBar.vue'
-import ApiCarCards from '../components/ApiCarCards.vue'
+import FilterCarCard from '../components/FilterCarCard.vue'
 
 import axios from 'axios'
 
@@ -45,27 +73,53 @@ export default {
   components: {
     Banner,
     NavBar,
-    ApiCarCards,
+    FilterCarCard,
   },
   data() {
     return {
       cars: [],
+      filtered_cars: [],
       filter: '',
       vehicle_type: [],
+      body_type: [],
+      car_seats: [],
     }
   },
-  async created(){
-    try {
-      const response = await axios.get('https://xpresspro-core.onrender.com/vehicles')
-      this.items = response.data;
-    } catch (error) {
-      console.error(error)
-    }
+  mounted() {
+    axios.get('https://xpresspro-core.onrender.com/vehicles')
+      .then(res => {
+        this.cars = res.data.data;
+      })
+      .catch(err => {
+        console.error(err);
+      })
   },
+  watch: {
+    vehicle_type() {
+      console.log(this.vehicle_type);
+
+      // this.filtered_cars = this.cars.filter(car => {
+      //   console.log('car');
+      // });
+    }
+  }
 }
 </script>
 
 <style scoped>
+.filters {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+
+.cars {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  gap: 12px;
+}
+
 .upper {
   background-image: url('/images/2.jpg');
   display: flex;
@@ -85,6 +139,7 @@ export default {
   padding: 40px 120px;
   display: flex;
   justify-content: space-between;
+  gap: 2px;
 }
 
 .filter {
